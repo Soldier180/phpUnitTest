61c6b70f23596fc3ac4c007c3110a9629d2c5931
Nikolay <Soldier180@gmail.com>
2017-12-18 14:51:53 +0200

    First test created

---
+++ src/Calculator.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nikolion
+ * Date: 18.12.2017
+ * Time: 14:36
+ */
+
+class Calculator
+{
+
+}
---
+++ test/CalculatorTest.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nikolion
+ * Date: 18.12.2017
+ * Time: 14:38
+ */
+
+use PHPUnit\Framework\TestCase;
+
+class CalculatorTest extends TestCase
+{
+
+}
