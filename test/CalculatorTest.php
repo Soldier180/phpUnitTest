c560e25d513a011d9f31d6532328e9b352bd212d
Nikolay <Soldier180@gmail.com>
2017-12-18 15:54:34 +0200

    phpunit xml settings added

--- test/CalculatorTest.php
+++ test/CalculatorTest.php
@@ -7,8 +7,18 @@
  */
 
 use PHPUnit\Framework\TestCase;
+require __DIR__."/../src/Calculator.php";
 
 class CalculatorTest extends TestCase
 {
 
+    public function addTest(){
+
+    }
+
+    public function test()
+    {
+        $calc = new Calculator();
+        $this->assertEquals(4,$calc->add(2,2),'Not right');
+    }
 }
